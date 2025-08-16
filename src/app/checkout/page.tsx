73e66ad770eb9e9f37bede87163e22d72b2c5094
BiTI <lethien1932003@gmail.com>
2025-08-16 15:14:07 +0700

    ok

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,5 +1,10 @@
-import CheckoutClient from '@/components/CheckoutClient';
+import { Suspense } from 'react';
+import CheckoutClient from '../../components/CheckoutClient';
 
 export default function CheckoutPage() {
-  return <CheckoutClient />;
+  return (
+    <Suspense fallback={<div>Đang tải...</div>}>
+      <CheckoutClient />
+    </Suspense>
+  );
 }
